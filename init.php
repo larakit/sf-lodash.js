581ac24ea008fa38ee56313493f541b2eefc48c6
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:43:58 +0500

    fixed

--- init.php
+++ init.php
@@ -1,3 +1,4 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-lodash.js')
-    ->js('//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.3.0/lodash.js');
+    ->setSourceDir('public')
+    ->jsPackage('lodash.min.js');
